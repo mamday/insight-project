e7ad62d0e01faa7884efcddb295e947d262e5ed8
Melanie Day <mamday@gmail.com>
2016-06-23 10:16:05 -0700

    Fix all the things I did not copy correctly

--- meetupw2vecscores.py
+++ meetupw2vecscores.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
@@ -8,7 +9,6 @@ import time
 import geopy 
 from geopy import *
 from geopy.distance import vincenty
-from __future__ import division
 from gensim import corpora, models, similarities
 from gensim.models import word2vec
 import re
@@ -75,19 +75,42 @@ if not database_exists(engine.url):
 con = None
 con = psycopg2.connect(database = dbname, user = username, host='localhost', password=pswd)
 
-nsim_gls = score_from_words(gnames,hn_model,nerd_vec)
-nsim_els = score_from_words(enames,hn_model,nerd_vec)
+user_cost=100
+import nltk
+from nltk.corpus import stopwords
+evt_query = "SELECT * FROM event_table,group_table WHERE event_table.group_url=group_table.group_url AND event_table.fee<%s" % user_cost
+query_results=pd.read_sql_query(evt_query,con)
+
+#group_topic = [clean_text(i,stop_bool=False).split(' ') for i in query_results['topic']]
+group_topic = query_results['topic']
+group_name = query_results['group_name']
+event_name = query_results['evt_name']
+
+topics = set(group_topic)
+stops = set(stopwords.words("english"))
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+gnames = [[tokenizer.tokenize(w)[0] for w in re.sub("[^a-zA-Z]", " ", i).lower().split() if not(w in stops)] for i in group_name]
+enames = [[tokenizer.tokenize(w)[0] for w in re.sub("[^a-zA-Z]", " ", i).lower().split() if not(w in stops)] for i in event_name]
+tnames=[[tokenizer.tokenize(w)[0] for w in re.sub("[^a-zA-Z]", " ", i).lower().split() if not(w in stops)] for i in group_topic]
+
+nsim_gls = score_from_words(gnames,hn_model,nerd_vec,False)
+nsim_els = score_from_words(enames,hn_model,nerd_vec,False)
+nnsim_els = score_from_words(enames,hn_model,nerd_vec)
+
+print len(nsim_gls),len(nsim_els),len(nnsim_els),nsim_gls[:10],nsim_els[:10],nnsim_els[:10]
 
 #Write scores to pandable file
-open('groupeventscores-1.csv','w')
-f1.write('evt_id,g_score,e_score\n')
-evts = {}
 
-for ind in xrange(len(nsim_gls)):
-    cur_e = query_results['evt_id'][ind]
-    if(not(int(cur_e) in evts)):
-        out_text = str(cur_e)+','+str(nsim_gls[ind])+','+str(nsim_els[ind])+'\n'
-        f1.write(out_text)
-    evts[int(cur_e)]=1
+#f1 = open('groupeventscores-1.csv','w')
+#f1.write('evt_id,g_score,e_score\n')
+#evts = {}
+
+#for ind in xrange(len(nsim_gls)):
+#    cur_e = query_results['evt_id'][ind]
+#    if(not(int(cur_e) in evts)):
+#        out_text = str(cur_e)+','+str(nsim_gls[ind])+','+str(nsim_els[ind])+','+str(nnsim_els)+'\n'
+#        f1.write(out_text)
+#    evts[int(cur_e)]=1
 
 
